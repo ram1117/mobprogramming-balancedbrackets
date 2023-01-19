f4e4a736b79c967f1c115106c85730ac22eb7457
ram kumar karuppusamy <ramkumark1@outlook.com>
2023-01-19 15:02:08 +0530

    add solution to braces challenge using stack

--- challenge.js
+++ challenge.js
@@ -1,5 +1,38 @@
+class Stack{
+  constructor(){
+    this.stackArr=[];
+  }
+  push(char){
+    this.stackArr.push(char);
+  }
+  pop(){
+    return this.stackArr.pop();
+  }
+  empty(){
+    return this.stackArr.length===0;
+  }
+}
+
 function balancedBrackets(string) {
   // your code here
+  const stck = new Stack();
+  let braces = string.match(/[\[\]\(\)\{\}]/g);
+
+  for(let i = 0;i<braces.length;i+=1){
+    if(braces[i]==='{'||braces[i]==='['||braces[i]==='('){
+      stck.push(braces[i]);
+    }
+    else{
+      if(stck.empty()){return false;}
+      let top = stck.pop();
+      if((top==='[' && braces[i]!==']')||(top==='(' && braces[i]!==')')||(top==='{' && braces[i]!=='}'))
+      {
+        return false;
+      }
+    }
+
+  }
+  return stck.empty();
 }
 
 console.log(balancedBrackets('(hello)[world]'))
