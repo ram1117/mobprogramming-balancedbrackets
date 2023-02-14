4aef6727582d43bce56cb4486625454c7a32689a
ram kumar karuppusamy <ramkumark1@outlook.com>
2023-02-14 13:02:41 +0530

    Solution for the challenge

--- challenge.js
+++ challenge.js
@@ -1,5 +1,30 @@
+class Stack {
+  constructor(){
+    this.arr=[];
+  }
+  popArr(){
+    return this.arr.pop();
+  }
+
+  pushArr(char){
+    this.arr.push(char);
+  }
+}
+
 function balancedBrackets(string) {
-  // your code here
+  
+let braceStr = string.match(/[\[\]\(\)\{\}]/g);
+for(let i=0;i<braceStr.length;i+=1){
+  let stack = new Stack();
+  if(braceStr[i]==='['||braceStr[i]==='('||braceStr[i]==='{'){ stack.pushArr(braceStr[i])}
+  else{
+    let head = stack.popArr();
+    if(head==='('&& braceStr[i]!==')'||head==='['&& braceStr[i]!==']'||head==='{'&& braceStr[i]!=='}'){
+      return false;
+    }
+    return stack.arr.isEmpty;
+  }
+}
 }
 
 console.log(balancedBrackets('(hello)[world]'))
