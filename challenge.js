82aaf98e417d1574bfc885fc2a1f734ebd627c38
ram kumar karuppusamy <ramkumark1@outlook.com>
2023-02-16 12:59:44 +0530

    fix the solution

--- challenge.js
+++ challenge.js
@@ -1,37 +1,47 @@
 class Stack {
-  constructor(){
-    this.arr=[];
+  constructor() {
+    this.arr = [];
   }
-  popArr(){
+  popArr() {
     return this.arr.pop();
   }
 
-  pushArr(char){
+  pushArr(char) {
     this.arr.push(char);
   }
+
+  isEmpty() {
+    return this.arr.length === 0;
+  }
+
 }
 
 function balancedBrackets(string) {
-  
-let braceStr = string.match(/[\[\]\(\)\{\}]/g);
-for(let i=0;i<braceStr.length;i+=1){
+
+  let braceStr = string.match(/[\[\]\(\)\{\}]/g);
   let stack = new Stack();
-  if(braceStr[i]==='['||braceStr[i]==='('||braceStr[i]==='{'){ stack.pushArr(braceStr[i])}
-  else{
-    let head = stack.popArr();
-    if(head==='('&& braceStr[i]!==')'||head==='['&& braceStr[i]!==']'||head==='{'&& braceStr[i]!=='}'){
-      return false;
+  for (let i = 0; i < braceStr.length; i += 1) {
+    if (braceStr[i] === '[' || braceStr[i] === '(' || braceStr[i] === '{') { stack.pushArr(braceStr[i]) }
+    else {
+      if (stack.arr.length===0) { return false }
+      else {
+        let head = stack.popArr();
+        if (head === '(' && braceStr[i] !== ')' || head === '[' && braceStr[i] !== ']' || head === '{' && braceStr[i] !== '}') {
+          return false;
+        }
+      }
+
     }
-    return stack.arr.isEmpty;
   }
-}
+  return stack.arr.length===0;
 }
 
 console.log(balancedBrackets('(hello)[world]'))
 // => true
-
 console.log(balancedBrackets('([)]'))
 // => false
+console.log(balancedBrackets(')]'))
+console.log(balancedBrackets('{()[]}()'))
 
 console.log(balancedBrackets('[({}{}{})([])]'))
 // => true
